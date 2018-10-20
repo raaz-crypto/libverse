7533592430135a4bdf5c8328fd6631e751d8037a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-10-20 23:28:38 +0530

    haskell: order of arguments fixed

--- Raaz/Verse/Chacha20/C/Portable.hs
+++ Raaz/Verse/Chacha20/C/Portable.hs
@@ -10,7 +10,7 @@ import Data.Word
 foreign import ccall unsafe
     verse_chacha20_c_portable :: Ptr (Ptr (Tuple 16 Word32))
                               -> Word64
-                              -> Ptr Word32
-                              -> Ptr (Tuple 3 Word32)
                               -> Ptr (Tuple 8 Word32)
+                              -> Ptr (Tuple 3 Word32)
+                              -> Ptr Word32
                               -> IO ()
