54811bd478cec36ac984c8b55f6591869206222e
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-12-03 16:38:33 +0530

    added a host endian variant of chacha20 keystream.
    There are two things that can possibly make this faster than the
    chacha20 blocks function.
    
    1. It uses the host endianness instead of little endian
    
    2. It only tries to generate the key stream instead of xoring with the
       key stream. While the cost of the xor might be negligible, there is
       a read that it should save.
    
    In practice, there might be no noticeable difference.

--- Raaz/Verse/Chacha20/C/Portable.hs
+++ Raaz/Verse/Chacha20/C/Portable.hs
@@ -14,3 +14,10 @@ foreign import ccall unsafe
                               -> Ptr (Tuple 3 Word32)
                               -> Ptr Word32
                               -> IO ()
+foreign import ccall unsafe
+    verse_chacha20_c_portable_keystream :: Ptr (Tuple 16 Word32)
+                                        -> Word64
+                                        -> Ptr (Tuple 8 Word32)
+                                        -> Ptr (Tuple 3 Word32)
+                                        -> Ptr Word32
+                                        -> IO ()
