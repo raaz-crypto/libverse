a5b4646e50a47c6b659acbe6998f6faf862bcc5a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-02-05 10:48:26 +0530

    poly1305 clamp now works on host endian

--- poly1305/c/portable.c
+++ poly1305/c/portable.c
@@ -490,12 +490,12 @@ void verse_poly1305_c_portable_clamp(uint64_t (*blockPtr)[2], uint64_t counter)
     /*    Iterating over the blocks    */
     while(counter > 0)
     {
-        rtemp =  verse_from_le64((*blockPtr)[0]);
+        rtemp =  (*blockPtr)[0];
         rtemp &= 0x0ffffffc0fffffffULL;
-        (*blockPtr)[0] = verse_to_le64( rtemp);
-        rtemp =  verse_from_le64((*blockPtr)[1]);
+        (*blockPtr)[0] =  rtemp;
+        rtemp =  (*blockPtr)[1];
         rtemp &= 0x0ffffffc0ffffffcULL;
-        (*blockPtr)[1] = verse_to_le64( rtemp);
+        (*blockPtr)[1] =  rtemp;
         ++blockPtr;
         --counter;
         /*    move to next block    */
