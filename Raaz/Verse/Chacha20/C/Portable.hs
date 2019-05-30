81850452a1f8548a9f3d1e2349cce9affdcc746c
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-05-30 16:08:59 +0530

    xchacha20 and friends

--- Raaz/Verse/Chacha20/C/Portable.hs
+++ Raaz/Verse/Chacha20/C/Portable.hs
@@ -12,9 +12,20 @@ foreign import ccall unsafe
                               -> Ptr Word32
                               -> IO ()
 foreign import ccall unsafe
-    verse_chacha20_c_portable_keystream :: Ptr (Tuple 16 Word32)
-                                        -> Word64
-                                        -> Ptr (Tuple 8 Word32)
-                                        -> Ptr (Tuple 3 Word32)
-                                        -> Ptr Word32
-                                        -> IO ()
+    verse_chacha20csprg_c_portable :: Ptr (Tuple 16 Word32)
+                                   -> Word64
+                                   -> Ptr (Tuple 8 Word32)
+                                   -> Ptr (Tuple 3 Word32)
+                                   -> Ptr Word32
+                                   -> IO ()
+foreign import ccall unsafe
+    verse_hchacha20_c_portable :: Ptr (Tuple 8 Word32)
+                               -> Ptr (Tuple 6 Word32)
+                               -> IO ()
+foreign import ccall unsafe
+    verse_xchacha20_c_portable :: Ptr (Tuple 16 Word32)
+                               -> Word64
+                               -> Ptr (Tuple 8 Word32)
+                               -> Ptr (Tuple 6 Word32)
+                               -> Ptr Word32
+                               -> IO ()
