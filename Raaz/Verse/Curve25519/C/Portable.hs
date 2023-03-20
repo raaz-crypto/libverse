fd5ff14270126e0e97bb474383ba34934ae1122b
Piyush P Kurur <ppk@iitpkd.ac.in>
2023-03-20 10:27:11 +0530

    DRAFT: x25519 key exchange

--- Raaz/Verse/Curve25519/C/Portable.hs
+++ Raaz/Verse/Curve25519/C/Portable.hs
@@ -3,6 +3,10 @@
 module Raaz.Verse.Curve25519.C.Portable where
 import Raaz.Core
 foreign import ccall unsafe verse_curve25519_c_portable_clamp
-        :: Ptr (Tuple 4 (LE Word64))
+        :: Ptr (Tuple 4 Word64)
         -> Word64
+        -> IO ()
+foreign import ccall unsafe verse_x25519_c_portable
+        :: Ptr (Tuple 4 Word64)
+        -> Ptr (Tuple 4 Word64)
         -> IO ()
